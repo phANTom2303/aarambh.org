cebfc7ee508c553bce3597c7853fbee7e1fcc18b
phaANTom2303 <anikavy.goenka@gmail.com>
2025-06-26 21:46:58 +0530

    temp commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,42 @@
-import Header from "./Header.jsx"
-import Mission from "./Mission.jsx"
-import Banner from "./Banner.jsx"
-import Intro from "./Intro.jsx"
-import Cta from "./Cta.jsx"
+// import Header from "./Header.jsx"
+// import Mission from "./Mission.jsx"
+// import Banner from "./Banner.jsx"
+// import Intro from "./Intro.jsx"
+
+import ArticleDashboard from "./pages/article dashboard/articleDashboard"
+import MemberDashboard from "./pages/memberDashboard/memberDashbaord"
+
+// import Article from "./Article"
+// import Cta from "./Cta.jsx"
+
 function App() {
-return (
-    <>
-    <Header></Header>
-     <Mission></Mission>
-     <Banner></Banner>
-     <Intro></Intro>
-     <Cta></Cta> 
-    </>
-  )
+    return (
+        <>
+
+            {/* <Header></Header>
+            <Mission></Mission>
+            <Banner></Banner>
+            <Intro></Intro>
+            <Cta></Cta> */}
+
+            {/* <MemberDashboard /> */}
+
+            <ArticleDashboard />
+
+            {/* <Article title={"Test Article"} image={"src/assets/logo.jpg"} summary={"This is a test article"} ></Article>
+            <hr />
+            <Article title={"Test Article"} image={"src/assets/logo.jpg"} summary={"This is a test article"} ></Article>
+            <hr />
+            <Article title={"Test Article"} image={"src/assets/logo.jpg"} summary={"This is a test article"} ></Article>
+            <hr />
+            <Article title={"Test Article"} image={"src/assets/logo.jpg"} summary={"This is a test article"} ></Article>
+            <hr />
+            <Article title={"Test Article"} image={"src/assets/logo.jpg"} summary={"This is a test article"} ></Article>
+            <hr />
+            <Article title={"Test Article"} image={"src/assets/logo.jpg"} summary={"This is a test article"} ></Article>
+            <hr /> */}
+        </>
+    )
 }
 
 export default App
