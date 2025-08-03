1d3b7c568568907f3eff31757a75806f0de52830
jaspreetjk20 <jaspreetsk.2020@gmail.com>
2025-08-03 18:02:50 +0530

    Added article component features

---
+++ client/src/pages/Articles_events/Article.jsx
@@ -0,0 +1,34 @@
+import styles from "./Article.module.css";
+import { useState } from "react";
+
+function Article({ title, image, date, content, carousel }) {
+
+  const[current, setCurrent] = useState(0);
+  const next = () => setCurrent((prev) => (prev+1)%carousel.length); //wraps around to first image
+  const prev = () => setCurrent((prev) => (prev-1+carousel.length)%carousel.length); //wraps around to last image
+
+  return (
+    <div className={styles.articlePage}>
+      <h1 className={styles.title}>{title}</h1>
+      <p className={styles.date}>📅 {date}</p>
+      <img className={styles.image} src={image} alt="Article" />
+      <div className={styles.content}>
+        {content.split("\n").map((para, i) => (
+          <p key={i}>{para}</p>
+        ))}
+      </div>
+
+      {carousel && carousel.length > 0 && (
+        <div className={styles.carousel}>
+          <button onClick={prev}>⟵</button>
+          <img
+          src ={carousel[current].src}
+          className={styles.carouselImage}/>
+          <button onClick={next}>⟶</button>
+    </div>
+      )}
+      </div>
+  );
+}
+
+export default Article;
