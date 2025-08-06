4c84862cb3e55da9151217daf9dde72ccd99534c
phaANTom2303 <anikavy.goenka@gmail.com>
2025-08-07 03:39:42 +0530

    fix:header icon leads to hompage now

--- client/src/pages/homepage/Header.jsx
+++ client/src/pages/homepage/Header.jsx
@@ -1,11 +1,15 @@
 import logo from "../../assets/logo.jpg";
 import styles from "./Header.module.css";
 import Cta from "./Cta.jsx";
+import { Link } from "react-router-dom";
+
 
 function Header() {
     return (
         <div className={styles.header}>
-            <img src={logo} alt="NGO logo" className={styles.logo} />
+            <Link to={'/'}>
+                <img src={logo} alt="NGO logo" className={styles.logo} />
+            </Link>
             <h1 className={styles.title}>Aarambh</h1>
             <Cta />
             {/*<div className={styles.spacer}></div>*/}
